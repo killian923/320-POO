bf9d3b78294ff9b898225524d613c0365bd1003e
killian923 <144005114+killian923@users.noreply.github.com>
2025-08-29 10:59:12 +0200

    Add initial snail console animation project

---
+++ personnel/snail/snail/snail/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace snail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.SetCursorPosition(i,0);
+                Snail();
+                Console.SetCursorPosition(i, 0);
+                Console.Write(" ");
+
+
+                Thread.Sleep(150);
+            }
+            Console.Write(" ____");
+            
+            Console.Read();
+
+        }
+        static void Snail()
+        {
+            Console.Write("  _@_ö");
+        }
+    }
+}
