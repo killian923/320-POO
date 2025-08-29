82a94652b9d6652ce4362a28007b7b1e6b627f9d
killian923 <144005114+killian923@users.noreply.github.com>
2025-08-29 11:38:37 +0200

    Snail version OO

--- personnel/snail/snail/snail/Program.cs
+++ personnel/snail/snail/snail/Program.cs
@@ -1,37 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Claims;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace snail
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            int i;
+
             Console.CursorVisible = false;
             Console.ForegroundColor = ConsoleColor.Blue;
 
-            for (int i = 0; i < 20; i++)
+            Snail snail = new Snail();
+            //Snail snail = new Snail("  ____@_ö", " _______", 300 ,30);
+
+
+            for (i = 0; i < snail._life; i++)
             {
-                Console.SetCursorPosition(i,0);
-                Snail();
                 Console.SetCursorPosition(i, 0);
-                Console.Write(" ");
-
-
-                Thread.Sleep(150);
+                Console.Write(snail._formLife);
+                Thread.Sleep(snail._speed);
             }
-            Console.Write(" ____");
-            
+            if (i == snail._life)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write(snail._formDead);
+            }
             Console.Read();
+        }
+    }
+    class Snail
+    {
+        public string _formLife;
+        public string _formDead;
+        public int _speed;
+        public int _life;
 
-        }
-        static void Snail()
+        public Snail()
         {
-            Console.Write("  _@_ö");
+            this._formLife = "  __@_ö";
+            this._formDead = "  _____";
+            this._speed = 250;
+            this._life = 21;
         }
+        public Snail(string form, string formDead, int speed, int life)
+        {
+            _formLife = form;
+            _formDead = formDead;
+            _speed = speed;
+            _life = life;
+        }
+
     }
 }
