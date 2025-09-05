359d9a9c962758b7ccd2380155cdcbb2d2ff54ba
killian923 <144005114+killian923@users.noreply.github.com>
2025-09-05 09:17:48 +0200

    Feat(plane)
    Affichage de l'avion

---
+++ personnel/ParaChute/ParaChute/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParaChute
+{
+    static class Config
+    {
+        public const int SCREEN_HEIGHT = 40;
+        public const int SCREEN_WIDTH = 150;
+    }
+
+
+    class Plane
+    {
+        private string[] view =
+        {
+            @" _                         ",
+            @"| \                        ",
+            @"|  \       ______          ",
+            @"--- \_____/  |_|_\____  |  ",
+            @"  \_______ --------- __>-} ",
+            @"        \_____|_____/   |  "
+        };
+        public void draw()
+        {
+            for (int i = 0; i < view.Length; i++)
+            {
+                Console.WriteLine(view[i]);
+            }
+        }
+       
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = Config.SCREEN_HEIGHT;
+            Console.WindowWidth = Config.SCREEN_WIDTH;
+
+            Plane plane = new Plane();
+
+            plane.draw();
+
+            Console.ReadLine();
+        }
+    }
+  
+}
