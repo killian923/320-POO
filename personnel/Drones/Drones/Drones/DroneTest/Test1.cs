2012368e9156ec0cfb32416d569991c01d130698
killian923 <144005114+killian923@users.noreply.github.com>
2025-10-01 14:28:05 +0200

    Feat(Shoot) : ajout du shoot du joueur

--- personnel/Drones/Drones/Drones/DroneTest/Test1.cs
+++ personnel/Drones/Drones/Drones/DroneTest/Test1.cs
@@ -1,12 +1,35 @@
-﻿namespace DroneTest
+﻿using Drones;
+
+namespace DroneTest
 {
     [TestClass]
     public class Drone
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_that_drone_is_taking_orders()
         {
+            // Arrange
+            Drone drone = new Drone(500, 500);
 
+            // Act
+            EvacuationState state = drone.GetEvacuationState();
+
+            // Assert
+            Assert.AreEqual(EvacuationState.Free, state);
+
+            // Arrange a no-fly zone around the drone
+            bool response = drone.Evacuate(new System.Drawing.Rectangle(400, 400, 200, 200));
+
+            // Assert
+            Assert.IsFalse(response); // because the zone is around the drone
+            Assert.AreEqual(EvacuationState.Evacuating, drone.GetEvacuationState());
+
+            // Arrange: remove no-fly zone
+            drone.FreeFlight();
+
+            // Assert
+            Assert.AreEqual(EvacuationState.Free, drone.GetEvacuationState());
         }
+
     }
 }
--- personnel/Drones/Drones/Drones/Drones/Model/Drone.cs
+++ personnel/Drones/Drones/Drones/Drones/Model/Drone.cs
@@ -3,13 +3,16 @@
 namespace Drones
 {
     // Cette partie de la classe Drone définit ce qu'est un drone par un modèle numérique
-    public partial class Drone
+    public partial class Drone : IExpellable
     {
         private const int FULLCHARGE = 1000;
         private int _charge;                     // La charge actuelle de la batterie
         private string _name;                           // Un nom
         private int _x;                                // Position en X depuis la gauche de l'espace aérien
         private int _y;                                 // Position en Y depuis le haut de l'espace aérien
+
+     
+
 
         public int X { get => _x; set => _x = value; }
         public int Y { get => _y; set => _y = value; }
@@ -27,10 +30,26 @@ namespace Drones
         // que 'interval' millisecondes se sont écoulées
         public void Update(int interval)
         {
-            X += 2;                                    // Il s'est déplacé de 2 pixels vers la droite
-            Y += nbrRandom.rnd.Next(-3, 4);                     // Il s'est déplacé d'une valeur aléatoire vers le haut ou le bas
-            _charge--;                                  // Il a dépensé de l'énergie
+           
+                X += 2;                                    // Il s'est déplacé de 2 pixels vers la droite
+                Y += nbrRandom.rnd.Next(-3, 4);                     // Il s'est déplacé d'une valeur aléatoire vers le haut ou le bas
+                _charge--;
+      
         }
 
+        public bool Evacuate(Rectangle zone)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void FreeFlight()
+        {
+            throw new NotImplementedException();
+        }
+
+        public EvacuationState GetEvacuationState()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
